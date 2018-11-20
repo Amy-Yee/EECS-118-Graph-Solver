b88afa943e4b487287a5c9f19b03a3de32cc0ccc
Amy Yee <ayee5@uci.edu>
2018-11-20 01:12:52 -0800

    Voronoi cells progress
    Creates graph with nodes colored according to Voronoi cells

--- vitality.py
+++ vitality.py
@@ -5,34 +5,39 @@ import numpy as np
 
 G = nx.Graph()
 
-with open(input("Please enter your filepath: ")) as csv_file:
-    reader = csv.reader(csv_file)
-    next(reader)
-    source = []
-    target = []
-    value = []
+# generates a fresh graph
+def newgraph(G):
+    G.clear()
+    with open(input("Please enter your filepath: ")) as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        source = []
+        target = []
+        value = []
 
-    for row in reader:
-        source.append(int(row[0]))
-        target.append(int(row[1]))
-        value.append(int(row[2]))
+        for row in reader:
+            source.append(int(row[0]))
+            target.append(int(row[1]))
+            value.append(int(row[2]))
 
-#add nodes to the graph
-for i in range(0, np.size(target) + 1):
-    G.add_node(i)
+    for i in range(0, np.size(target) + 1):
+        G.add_node(i)
 
-#add weighted edges to the graph
-for i in range(np.size(source)):
-    G.add_weighted_edges_from([(source[i], target[i], value[i])])
-    print(source[i],target[i],value[i])
+    for i in range(np.size(source)):
+        G.add_weighted_edges_from([(source[i], target[i], value[i])])
+        print(source[i],target[i],value[i])
 
 
-selected_node = int(input("Enter the vitality of the node you wish to see: "))
-print(nx.closeness_vitality(G,selected_node))
+def vitality(G):
+    newgraph(G)
+    selected_node = int(input("Enter the vitality of the node you wish to see: "))
+    print(nx.closeness_vitality(G,selected_node))
 
-# shows graph with the node removed
-G.remove_node(selected_node)
-pos = nx.spring_layout(G)
-nx.draw(G, pos, with_labels=True, node_color='b', edge_color='k', node_size=200, alpha=0.5)
-pylab.title('Self_Define Net', fontsize=15)
-pylab.show()
+    # shows graph with the node removed
+    G.remove_node(selected_node)
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_color='b', edge_color='k', node_size=200, alpha=0.5)
+    pylab.title('Self_Define Net', fontsize=15)
+    pylab.show()
+
+vitality(G)
--- voronoi.py
+++ voronoi.py
@@ -1,36 +1,60 @@
 import pylab
-import matplotlib.cm as cm
 import networkx as nx
+import csv
+import numpy as np
+import matplotlib.cm as cm
 
-
-# Generates the nodes
-G = nx.path_graph(40)
-center_nodes = {0, 2, 27}
-cells = nx.voronoi_cells(G, center_nodes)
-partition = set(map(frozenset, cells.values()))
-sorted(map(sorted, partition))
-temp_list = list(map(sorted,partition))
-
+# placeholder graph
+G = nx.DiGraph()
+center_nodes = []
 color_map = []
 
-A = nx.DiGraph()
+def main():
+    # generate a fresh DiGraph
+    generatedigraph(G)
 
-ctr_node_list = list(center_nodes)
+    # user inputs center nodes
+    node = input("Please enter a center node, or press ENTER to quit: ")
+    while (node != ""):
+        center_nodes.append(int(node))
+        node = input("Please enter a center node, or press ENTER to quit: ")
 
-# for center_node in ctr_node_list:
-#     A.add_node(center_node)
-#     color_map.append('black')
+    # obtains the Voronoi cells
+    cells = nx.voronoi_cells(G, center_nodes)
+    partition = set(map(frozenset,cells.values()))
+    cells_list = list(sorted(map(sorted, partition)))
+    print(cells_list)
 
-# add nodes to the graph in corresponding colors
-for i in range(0,len(center_nodes)):
-    nodes = temp_list[i]
-    for node in nodes:
-        A.add_node(node)
-        color_map.append(cm.hsv(1/(i+.5)))
+    # adds color corresponding to Voronoi cell
+    for i in range(0, len(cells_list)):
+        for j in range(0, len(cells_list[i])):
+            color_map.append(cm.hsv(1 / (i + .5)))
 
+    pos = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G, pos, with_labels=True, node_color=color_map, edge_color='k', node_size=200, alpha=0.5)
+    pylab.title('Voronoi cells', fontsize=15)
+    pylab.show()
 
+def generatedigraph(G):
+    G.clear()
 
-pos = nx.spring_layout(A)
-nx.draw_networkx_nodes(A, pos, node_color=color_map, with_labels=True)
-pylab.title('Voronoi cells', fontsize=15)
-pylab.show()
+    with open(input("Please enter your filepath: ")) as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        source = []
+        target = []
+        value = []
+
+        for row in reader:
+            source.append(int(row[0]))
+            target.append(int(row[1]))
+            value.append(int(row[2]))
+
+    for i in range(0, np.size(target) + 1):
+        G.add_node(i)
+
+    for i in range(np.size(source)):
+        G.add_weighted_edges_from([(source[i], target[i], value[i])])
+        print(source[i],target[i],value[i])
+
+main()
